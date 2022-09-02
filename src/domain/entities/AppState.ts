9a47752efd07b1b8c0058fd0103bf03d3495687e
Arnau Sanchez <pyarnau@gmail.com>
2022-09-02 12:00:48 +0200

    More

--- src/domain/entities/AppState.ts
+++ src/domain/entities/AppState.ts
@@ -1,3 +1,4 @@
+import { reducer } from "../../libs/reducer";
 import { Maybe } from "../../libs/ts-utils";
 import { Counter } from "./Counter";
 
@@ -11,3 +12,9 @@ export interface AppState {
 type Page = { type: "home" | "counter" };
 
 type Session = { type: "notLogged" } | { type: "logged"; username: string };
+
+const _appReducer = reducer<AppState>()({
+    setPage: (page: Page) => state => ({ ...state, page }),
+    logout: () => state => ({ ...state, session: { type: "notLogged" } }),
+    setCounter: (counter: Counter) => state => ({ ...state, counter }),
+});
