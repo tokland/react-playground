abff39cd6df6c2adfbd4aa779b4ec5547ab98b02
Arnau Sanchez <pyarnau@gmail.com>
2022-09-17 22:34:53 +0200

    More

--- src/libs/effect.ts
+++ src/libs/effect.ts
@@ -3,17 +3,15 @@ import { CancellablePromise, Cancellation } from "real-cancellable-promise";
 export type Cancel = () => void;
 
 export interface Effect<T> {
-    run(success: (data: T) => void, error: (msg: string) => void): Cancel;
+    run(success: (data: T) => void, reject: (msg: string) => void): Cancel;
 }
 
-export function cancellablePromiseToEffect<T>(cPromise: CancellablePromise<T>): Effect<T> {
+export function cancellablePromiseToEffect<T>(promise: CancellablePromise<T>): Effect<T> {
     return {
-        run(success, error) {
-            return cPromise.then(success).catch(err => {
-                if (err instanceof Cancellation) {
-                    console.log("Promise cancelled");
-                } else {
-                    error(err);
+        run(success, reject) {
+            return promise.then(success).catch(err => {
+                if (!(err instanceof Cancellation)) {
+                    reject(err);
                 }
             }).cancel;
         },
