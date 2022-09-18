be09599fd782c40609102e3964942c9219c3ace6
Arnau Sanchez <pyarnau@gmail.com>
2022-09-18 23:30:32 +0200

    More

--- src/webapp/pages/CounterPage.tsx
+++ src/webapp/pages/CounterPage.tsx
@@ -5,15 +5,39 @@ import Button from "../components/Button";
 import Counter from "../components/Counter";
 import Session from "../components/Session";
 import { useCancellableEffect } from "../hooks/useCancellableEffect";
+import create, { StoreApi } from "zustand";
+
+type StoreFrom<State, Actions> = State & { [K in keyof Actions]: Actions[K] };
+type Api = StoreApi<CounterState>;
+
+type CounterState = { value: number };
+
+class CounterActions {
+    constructor(private get: Api["getState"], private set: Api["setState"]) {}
+
+    addCounter = (n: number) => this.set(state => ({ value: state.value + n }));
+    incrementCounter = () => this.addCounter(+1);
+    decrementCounter = () => this.addCounter(-1);
+}
+
+const initialState: CounterState = { value: 0 };
+
+const useCounterStore = create<StoreFrom<CounterState, CounterActions>>((set, get) => {
+    return { ...initialState, ...new CounterActions(get, set) };
+});
 
 const CounterPage: React.FC = () => {
     const { actions } = useAppContext();
+    const state = useCounterStore(state => state);
 
     return (
         <>
             <Session />
             <Button onClick={actions.routes.goToHome} text="Back to Home Page" />
             <CurrentCounter />
+            <div>{state.value}</div>
+            <button onClick={state.incrementCounter}>INC</button>
+            <button onClick={state.decrementCounter}>DEC</button>
         </>
     );
 };
