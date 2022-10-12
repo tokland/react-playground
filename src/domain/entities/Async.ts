91f4d6c90640c5c68eec9ee1787197f65cec8ef7
Arnau Sanchez <pyarnau@gmail.com>
2022-10-12 20:38:17 +0200

    More

--- src/domain/entities/Async.ts
+++ src/domain/entities/Async.ts
@@ -7,26 +7,48 @@ import {
 export class Async<T> {
     private constructor(private _promise: () => CancellablePromise<T>) {}
 
-    static of<T>(data: T): Async<T> {
+    static success<T>(data: T): Async<T> {
         return new Async(() => CancellablePromise.resolve(data));
+    }
+
+    static error<T>(message: string, name = "GenericError"): Async<T> {
+        return new Async(() => CancellablePromise.reject(buildError(name, message)));
+    }
+
+    static fromComputation<T>(computation: Computation<T>): Async<T> {
+        let cancel: Cancel;
+
+        return new Async(() => {
+            const promise = new Promise<T>((resolve, reject) => {
+                cancel = computation(resolve, reject);
+            });
+
+            return new CancellablePromise(promise, cancel);
+        });
+    }
+
+    run(onSuccess: (data: T) => void, onError: (msg: string) => void): Cancel {
+        return this._promise().then(onSuccess, err => {
+            if (err instanceof Cancellation) {
+                // noop
+            } else if (err instanceof Error) {
+                onError(err.message);
+            } else {
+                onError("Unknown error");
+            }
+        }).cancel;
+    }
+
+    map<U>(fn: (data: T) => U): Async<U> {
+        return new Async(() => this._promise().then(fn));
+    }
+
+    flatMap<U>(fn: (data: T) => Async<U>): Async<U> {
+        return new Async(() => this._promise().then(data => fn(data)._promise()));
     }
 
     toPromise(): Promise<T> {
         return this._promise();
-    }
-
-    run(success: (data: T) => void, reject: (msg: string) => void): Cancel {
-        return this._promise()
-            .then(success)
-            .catch(err => {
-                if (!(err instanceof Cancellation)) {
-                    reject(err);
-                }
-            }).cancel;
-    }
-
-    map<U>(mapper: (data: T) => U): Async<U> {
-        return new Async(() => this._promise().then(mapper));
     }
 
     static delay(ms: number) {
@@ -34,19 +56,39 @@ export class Async<T> {
     }
 
     static void(): Async<void> {
-        return new Async(() => CancellablePromise.resolve(undefined));
+        return Async.success(undefined);
     }
 
     static block<U>(blockFn: (captureAsync: CaptureAsync) => Promise<U>): Async<U> {
-        const result: CancellablePromise<U> = buildCancellablePromise(capturePromise => {
-            const captureAsync: CaptureAsync = async => capturePromise(async._promise());
-            return blockFn(captureAsync);
+        return new Async((): CancellablePromise<U> => {
+            return buildCancellablePromise(capturePromise => {
+                const captureAsync: CaptureAsync = async => capturePromise(async._promise());
+                captureAsync.error = (msg, name) =>
+                    capturePromise(CancellablePromise.reject(new Error(msg)));
+                return blockFn(captureAsync);
+            });
         });
-
-        return new Async(() => result);
     }
 }
 
-type CaptureAsync = <T>(async: Async<T>) => Promise<T>;
+function buildError(name: string, message: string): AsyncError {
+    return { name, message, stack: new Error().stack || "" };
+}
+
+interface AsyncError {
+    name: string;
+    message: string;
+    stack: string;
+}
+
+export type Computation<Data> = (
+    resolve: (value: Data) => void,
+    reject: (error: AsyncError) => void
+) => Cancel;
+
+interface CaptureAsync {
+    <T>(async: Async<T>): Promise<T>;
+    error: <T>(message: string, name?: string) => Promise<T>;
+}
 
 type Cancel = () => void;
--- src/domain/entities/__tests/Async.spec.ts
+++ src/domain/entities/__tests/Async.spec.ts
@@ -1,24 +1,33 @@
 import { Async } from "../Async";
 
-const async1 = Async.of(1);
-
-test("of", async () => {
-    const value1 = await successOf(async1);
-    expect(value1).toEqual(1);
+test("success", async () => {
+    const value = await successOf(Async.success(10));
+    expect(value).toEqual(10);
 });
 
-test("run", async () => {
-    const asyncValue = Async.of(1);
-
+test("run with success", async () => {
+    const asyncValue = Async.success(1);
     const success = jest.fn();
     const reject = jest.fn();
-    asyncValue.run(success, reject);
 
+    asyncValue.run(success, reject);
     await new Promise(process.nextTick);
 
     expect(success).toHaveBeenCalledTimes(1);
     expect(reject).not.toHaveBeenCalled();
     expect(success.mock.calls).toEqual([[1]]);
+});
+
+test("run with error", async () => {
+    const asyncValue = Async.error("err");
+    const success = jest.fn();
+    const reject = jest.fn();
+    asyncValue.run(success, reject);
+    await new Promise(process.nextTick);
+
+    expect(success).not.toHaveBeenCalled();
+    expect(reject).toHaveBeenCalledTimes(1);
+    expect(reject.mock.calls).toEqual([["err"]]);
 });
 
 test("void", async () => {
@@ -34,21 +43,53 @@ test("delay", async () => {
 });
 
 test("map", async () => {
+    const async1 = Async.success(1);
     const async4 = async1.map(x => x + 3);
     const value4 = await successOf(async4);
+
     expect(value4).toEqual(4);
 });
 
-test("block", async () => {
-    const asyncValue = Async.block(async capture => {
-        const value1 = await capture(Async.of(1));
-        const value2 = await capture(Async.of(2));
-        const value3 = await capture(Async.of(3));
-        return value1 + value2 + value3;
+test("flatMap", async () => {
+    const value1$ = Async.success(1);
+    const value2$ = value1$
+        .flatMap(value => Async.success(value + 2))
+        .flatMap(value => Async.success(value + 3));
+
+    expect(value2$.toPromise()).resolves.toEqual(6);
+});
+
+describe(".block", () => {
+    test("returns successful async value", async () => {
+        const asyncValue = Async.block(async $ => {
+            const value1 = await $(Async.success(1));
+            const value2 = await $(Async.success(2));
+            const value3 = await $(Async.success(3));
+            return value1 + value2 + value3;
+        });
+
+        expect(asyncValue.toPromise()).resolves.toEqual(6);
     });
 
-    const value = await successOf(asyncValue);
-    expect(value).toEqual(6);
+    test("returns an async error if an intermediate awaited value fails", async () => {
+        const asyncValue = Async.block(async $ => {
+            const value1 = await $(Async.success(1));
+            const value2 = await $(Async.error<number>("Error message"));
+            const value3 = await $(Async.success(3));
+            return value1 + value2 + value3;
+        });
+
+        expect(asyncValue.toPromise()).rejects.toThrow("Error message");
+    });
+
+    test("returns an async error if $.error is returned in the block", async () => {
+        const asyncValue = Async.block(async ($): Promise<number> => {
+            if (1 + 2 > 1) return $.error("Error message");
+            return $(Async.success(1));
+        });
+
+        expect(asyncValue.toPromise()).rejects.toThrow("Error message");
+    });
 });
 
 async function successOf<T>(asyncValue: Async<T>): Promise<T> {
